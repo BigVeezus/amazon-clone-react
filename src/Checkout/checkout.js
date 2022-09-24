e387bc331e2f2bacf7819ceaff28d111ce79357d
Elvis Osuji <97410294+BigVeezus@users.noreply.github.com>
2022-09-24 03:59:52 +0100

    Added checkout page

--- src/Checkout/checkout.js
+++ src/Checkout/checkout.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import './checkout.css'
+import CheckoutProduct from './checkoutProduct';
+import Subtotal from '../Subtotal/subtotal';
 
 
 function Checkout() {
   return (
     <div className='checkout'>
       <div className="checkout-left">
-      
-      </div>
+        <img src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg' alt='' className='checkout-ad'/>
+        <div>
+          <h2 className='checkout-title'>Your Shopping Basket</h2>
+          <CheckoutProduct/>
+        </div>
+        </div>
       <div className="checkout-right">
-      
+        <Subtotal/>
       </div>
     </div>
   )
